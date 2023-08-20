fb4883ef3536cc550822e1cfa16f44d31771c9a2
fedjes <salvados78@mail.ru>
2023-08-20 15:02:31 +0300

    filter

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -47,7 +47,8 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    // if(users.myFriends.address.city === 'Los Angeles')
+    const filteredUsers = users.myFriends.filter( (f)=> {f.address.city === 'Los Angeles'}) //'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
